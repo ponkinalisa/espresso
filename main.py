b8a4d6fd5eb3b6d02ffe7216516f083d654eb2f6
Лиза <elisaveta.ponkina@yandex.ru>
2024-12-08 19:18:55 +0700

    обновление

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sqlite3
 from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
 from PyQt6 import uic
 
+
 class Add(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -23,47 +24,48 @@ class Add(QMainWindow):
                 l4 = 0
             self.con = sqlite3.connect("coffee.sqlite")
             self.cur = self.con.cursor()
-            id_tab = self.cur.execute(f"SELECT * FROM coffee WHERE ID = {id}").fetchone()
+            id_tab = self.cur.execute(f"SELECT * FROM cof WHERE ID = {id}").fetchone()
             if id_tab is None:
-                print(f"INSERT INTO coffee(ID, sort, roasting, ground_in_grains, description, price, volume) "
-                      f"VALUES({id}, '{sort}', '{l1}', '{l2}', '{l3}', {l4}, '{l5}')")
                 self.cur.execute(
-                    f"INSERT INTO coffee(ID, sort, roasting, ground_in_grains, description, price, volume) "
+                    f"INSERT INTO cof(ID, sort, roasting, ground_in_grains, description, price, volume) "
                     f"VALUES({id}, '{sort}', '{l1}', '{l2}', '{l3}', {l4}, '{l5}')")
             else:
-                print(f"UPDATE coffee "
-                      f"set ID = {id}, sort = '{sort}', roasting = '{l1}', ground_in_grains = '{l2}', "
-                      f"description = '{l3}', price = {l4}, volume = '{l5}' "
-                      f"WHERE ID = {id}")
-                self.cur.execute(f"UPDATE coffee "
+                self.cur.execute(f"UPDATE cof "
                                  f"SET ID = {id}, sort = '{sort}', roasting = '{l1}', ground_in_grains = '{l2}', "
                                  f"description = '{l3}', price = {l4}, volume = '{l5}' "
                                  f"WHERE ID = {id}")
+            self.con.commit()
             self.con.close()
         except Exception as e:
             print('%s' % e)
 
+
 class Caputino(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.w = None
         uic.loadUi('main.ui', self)
         self.get.clicked.connect(self.run)
+        self.add.clicked.connect(self.run1)
 
     def run(self):
         self.con = sqlite3.connect("coffee.sqlite")
         self.cur = self.con.cursor()
-        result = self.cur.execute("SELECT * FROM coffee").fetchall()
+        result = self.cur.execute("SELECT * FROM cof").fetchall()
         self.tableWidget.setRowCount(len(result))
         for i, elem in enumerate(result):
             for j, val in enumerate(elem):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
         self.con.close()
 
+    def run1(self):
+        if self.w is None:
+            self.w = Add()
+        self.w.show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Caputino()
-    es = Add()
     ex.show()
-    es.show()
     sys.exit(app.exec())
